7c6b66eff0900c33d3fb2fafea2debe6eb8bb584
j8s0nn <23lequocthinh06@gmail.com>
2026-08-28 17:58:25 -0600

    Add Creator page and component

---
+++ src/components/CreatorAdd.jsx
@@ -0,0 +1,105 @@
+import {useState} from 'react';
+import { supabase } from '../client';
+import {Link} from "react-router-dom"
+
+
+function CreatorAdd(){
+
+
+  const [image, setImage] = useState(null);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setUrl] = useState("");
+  const [id, setID] = useState(-1);
+
+
+  const handleImageChange = (e) => {
+    setImage(e.target.files[0]);
+  }
+
+  
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    const filePath = `creators/${crypto.randomUUID()}-${image.name}`;
+      // Upload image to storage
+      const {error: errorImage } =
+        await supabase.storage
+          .from("images")
+          .upload(filePath, image);
+
+      if(errorImage){
+        // console.log("error in image")
+        // console.log("Message:", errorImage.message);
+        alert("Cannot upload your photo");
+        return;
+      }
+
+    // Get the publicURL for displaying the information
+    const result = supabase.storage.from("images").getPublicUrl(filePath);
+    const publicURL = result.data.publicUrl;
+
+    // Get the id for viewing instantly
+    const { data,error} = await supabase.from("creators").insert({name: name, description: description, url: url, imageURL: publicURL}).select();
+
+    if(error){
+      alert("Cannot save your creator. Try again");
+      console.log(error.message)
+      return;
+    }
+
+    console.log(data[0].id)
+    setID(data[0].id)
+    alert("Succesfully create your creator");
+
+  }
+
+  return <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Creator name</label>
+        <input type="text" id="name" value={name} onChange={ (e) => {setName(e.target.value)}}/> 
+
+        <label htmlFor="description">Creator description</label>
+        <textarea
+          name="description"
+          id="description"
+          value={description}
+          onChange={(e) => {setDescription(e.target.value)}}
+        >
+          Type your description here
+        </textarea>
+
+
+        <label htmlFor="image">Creator image</label>
+        <input type="file" id="image" name="image" accept="image/jpeg, image/png, image/webp" onChange={handleImageChange}/>
+
+
+
+        <label htmlFor="url"></label>
+        <textarea
+          name="url"
+          id="url"
+          value={url}
+          onChange={(e) => {setUrl(e.target.value)}}
+        >
+          Type your creator's url here
+        </textarea>
+
+        
+
+
+        <button type="submit">Save changes</button>
+
+        
+       
+      </form>
+      
+      <Link to={`/creators/${id}`}> View your creator</Link>
+    </div>
+
+
+
+  
+}
+
+export default CreatorAdd;
--- src/pages/AddCreator.jsx
+++ src/pages/AddCreator.jsx
@@ -1,7 +1,14 @@
 //* This page is used when people hit the Add creator at home page
+import CreatorAdd from "../components/CreatorAdd";
+
 
 function AddCreator(){
-  return <h1>Add Creator</h1>
+  return <div>
+    <h1>Add Creator</h1>
+
+    <CreatorAdd />
+    
+    </div>
 }
 
 export default AddCreator;
