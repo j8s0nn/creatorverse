14ebac294c5792cac28311401a4dbeeff034b168
j8s0nn <23lequocthinh06@gmail.com>
2026-08-29 17:44:48 -0600

    Adjust the desgin for image + Effect on the card

--- src/components/CreatorCard.jsx
+++ src/components/CreatorCard.jsx
@@ -7,7 +7,9 @@ function CreatorCard({id,name, imageURL}){
   console.log(imageURL);
   return(
     <div className="creator-card-main">
-      <img className="creator-card-img" src={imageURL} alt={`${name} image`} />
+      <div className="creator-card-img-container">
+        <img className="creator-card-img" src={imageURL} alt={`${name} image`} />
+      </div>
       <p className="creator-card-name">{name}</p>
 
       <Link className="view-more-details-button" to={`creators/${id}`}>View more details</Link>
