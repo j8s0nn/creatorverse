fbad3025e172e9dbfd7d9d3299cbd08fd46b8e09
j8s0nn <23lequocthinh06@gmail.com>
2026-08-28 18:13:54 -0600

    Add creator delete confirmation

--- src/components/CreatorDetails.jsx
+++ src/components/CreatorDetails.jsx
@@ -1,7 +1,15 @@
 import {Link} from 'react-router-dom';
+import {useState} from 'react';
 
 
 function CreatorDetails({id,name, url, imageURL, description}){
+
+  const [isPopup, setIsPopup] = useState(false);
+  
+  function deleteCreator(){
+
+  }
+
   return (
     <div>
       
@@ -10,6 +18,22 @@ function CreatorDetails({id,name, url, imageURL, description}){
       <p>{description}</p>
       <p>URL: {url}</p>
       <Link to={`/creators/${id}/edit`}>Edit creator</Link>
+      <button onClick={ () => {setIsPopup(true)}}>Delete</button>
+
+      {isPopup && (
+        <div>
+          <p>Are you sure you want to delete this creator?</p>
+
+          <button onClick={deleteCreator}>
+            Yes
+          </button>
+
+          <button onClick={() => {setIsPopup(false)}}>
+            No
+          </button>
+        </div>
+      )}
+    
     </div>
   )
 }
