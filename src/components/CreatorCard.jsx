6a77ad0d2fdc4a085af4fe7fd41fe2a93de2a40e
j8s0nn <23lequocthinh06@gmail.com>
2026-08-29 16:53:46 -0600

    Style for creator card

--- src/components/CreatorCard.jsx
+++ src/components/CreatorCard.jsx
@@ -1,14 +1,16 @@
 
 import {Link} from "react-router-dom"
+import "./CreatorCard.css";
+
 
 function CreatorCard({id,name, imageURL}){
   console.log(imageURL);
   return(
-    <div>
-      <img src={imageURL} alt={`${name} image`} />
-      <p>{name}</p>
+    <div className="creator-card-main">
+      <img className="creator-card-img" src={imageURL} alt={`${name} image`} />
+      <p className="creator-card-name">{name}</p>
 
-      <Link to={`creators/${id}`}>View more details</Link>
+      <Link className="view-more-details-button" to={`creators/${id}`}>View more details</Link>
     </div>
 
   )
