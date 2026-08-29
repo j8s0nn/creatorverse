e67cb7a0973d685b3fcc98b0f75a787e15c7b7c4
j8s0nn <23lequocthinh06@gmail.com>
2026-08-29 17:45:19 -0600

    Style the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@
 import CreatorCard from "../components/CreatorCard";
 import { supabase } from "../client";
 import {useEffect, useState} from "react"
+import {Link} from "react-router-dom"
+import "./Home.css"
+
 function Home(){
 
   const [creators, setCreator] = useState([]);
@@ -28,21 +31,39 @@ function Home(){
 
 
   return <div>
-    <h1>Home</h1>
 
-    {creators.length === 0 
-      ? <p>Add your favorite creators</p>
-      :creators.map( (creator) => {
-        return (
-          <CreatorCard 
-            key={creator.id}
-            id={creator.id}
-            name={creator.name}
-            imageURL={creator.imageURL}       
-          />
-        )
-      } )   
-    }
+  <div className="home-container">
+    <div className="header-container">
+        <div className="header-info">
+          <h1>My favorite Creators</h1>
+          <h2>Discover and keep track of your creators</h2>
+        </div>
+      
+      <Link className="add-creator-button" to="creators/new">Add your creator</Link>
+
+    </div>
+    <div className="creators-container">
+      {creators.length === 0 
+        ? <p>Add your favorite creators</p>
+        :creators.map( (creator) => {
+          return (
+            <CreatorCard 
+              key={creator.id}
+              id={creator.id}
+              name={creator.name}
+              imageURL={creator.imageURL}       
+            />
+          )
+        } )   
+      }
+
+    </div>
+
+  </div>
+  
+    
+
+    
 
   </div>
 }
